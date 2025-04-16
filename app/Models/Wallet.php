916d348d042a72ee2d1937244f5b7111e9332de0
Charles <charlesbeasse1@gmail.com>
2025-04-16 14:46:00 +0200

    Feature: add notification for sending mail when balance is low

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Notifications\WalletLow;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,15 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Wallet extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::updating(function ($wallet) {
+            if ($wallet->balance < 1000) {
+                $wallet->user->notify(new WalletLow);
+            }
+        });
+    }
 
     /**
      * @return BelongsTo<User>
